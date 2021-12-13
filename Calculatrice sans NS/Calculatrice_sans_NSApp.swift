f298e104da5e95d7d63397cb6487b811fddbb777
Bilal <yyybilal@gmail.com>
2021-12-13 15:30:56 +0100

    Initial Commit

---
+++ Calculatrice sans NS/Calculatrice_sans_NSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculatrice_sans_NSApp.swift
+//  Calculatrice sans NS
+//
+//  Created by bilal on 13/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Calculatrice_sans_NSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculatrice sans NS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculatrice sans NS
+//
+//  Created by bilal on 13/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
